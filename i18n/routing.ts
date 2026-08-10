7acea3d807d66be04115b04cc391776bdf5fc5fe
nerina_berthelot <nerinaberthelot@gmail.com>
2026-08-10 16:43:48 +0200

    feat: localized pathnames for /projects and /projects/ABC-Comunidad

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,5 +2,15 @@ import {defineRouting} from 'next-intl/routing';
 
 export const routing = defineRouting({
   locales: ['en', 'es'],
-  defaultLocale: 'en'
+  defaultLocale: 'en',
+  pathnames: {
+    '/projects': {
+      en: '/projects',
+      es: '/proyectos'
+    },
+    '/projects/ABC-Comunidad': {
+      en: '/projects/ABC-Comunidad',
+      es: '/proyectos/ABC-Comunidad'
+    }
+  }
 });
